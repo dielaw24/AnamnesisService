b8593ff2a5b1761fe92bc9ae01289fdef848dc68
dielaw24 <dielaw24@users.noreply.github.com>
2020-07-09 01:36:35 -0500

    tabla diagnostico presuntivo

---
+++ src/main/java/com/anamnesis/model/PresumptiveDiagnostic.java
@@ -0,0 +1,32 @@
+package com.anamnesis.model;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+
+public class PresumptiveDiagnostic {
+    @Id
+    @Column(name = "id_diagnostico_presuntivo")
+    private int id_diagnostic;
+    @Column(name = "diagnostico_presuntivo")
+    private String presumptive_diagnostic;
+    @Column(name = "id_anamnesis")
+    private int id_anamnesis;
+    @Column(name = "id_enfermedad")
+    private int id_disease;
+
+    public int getId_diagnostic() {
+        return id_diagnostic;
+    }
+
+    public String getPresumptive_diagnostic() {
+        return presumptive_diagnostic;
+    }
+
+    public int getId_anamnesis() {
+        return id_anamnesis;
+    }
+
+    public int getId_disease() {
+        return id_disease;
+    }
+}
--- src/main/java/com/anamnesis/model/SymptomsGroup.java
+++ src/main/java/com/anamnesis/model/SymptomsGroup.java
@@ -1,5 +1,24 @@
 package com.anamnesis.model;
 
-public class SymptomsGroup {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "Grupo_Sintoma")
+public class SymptomsGroup {
+    @Id
+    @Column(name = "id_grupo_sintoma")
+    private int id_symptom_group;
+    @Column(name = "grupo_sintoma")
+    private String symptom_group;
+
+    public int getId_symptom_group() {
+        return id_symptom_group;
+    }
+
+    public String getSymptom_group() {
+        return symptom_group;
+    }
 }
