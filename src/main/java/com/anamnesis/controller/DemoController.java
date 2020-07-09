5a142a54eb1d269738e37d50b9f20f435b8aa6e9
dielaw24 <dielaw24@users.noreply.github.com>
2020-07-08 21:01:50 -0500

    test controller

--- src/main/java/com/anamnesis/controller/DemoController.java
+++ src/main/java/com/anamnesis/controller/DemoController.java
@@ -1,5 +1,15 @@
 package com.anamnesis.controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 
 public class DemoController {
-    
+    @GetMapping("/index")
+    public ResponseEntity<String> get() {
+        return ResponseEntity.ok("Hello World!");
+    }
 }
