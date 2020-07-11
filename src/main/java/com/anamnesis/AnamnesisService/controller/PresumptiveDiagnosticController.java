023afb321d96a897516c06abc1e163940ac55dc7
dielaw24 <dielaw24@users.noreply.github.com>
2020-07-11 13:03:59 -0500

    terminado rest diagnostico presumptivo

---
+++ src/main/java/com/anamnesis/AnamnesisService/controller/PresumptiveDiagnosticController.java
@@ -0,0 +1,51 @@
+package com.anamnesis.AnamnesisService.controller;
+
+import com.anamnesis.AnamnesisService.model.PresumptiveDiagnostic;
+import com.anamnesis.AnamnesisService.repository.PresumptiveDiagnosticRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping("/api/diagnostic")
+@CrossOrigin(origins="*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE, RequestMethod.PUT})
+public class PresumptiveDiagnosticController {
+
+    @Autowired
+    PresumptiveDiagnosticRepository presumptiveDiagnosticRepository;
+
+    //Obtener todos los diagnosticos presuntivos
+    @GetMapping
+    public Iterable<PresumptiveDiagnostic> getAllDiagnostic(){
+        return presumptiveDiagnosticRepository.findAll();
+    }
+    //Obtener  diagnosticos presuntivos por id
+    @GetMapping("/{id_diag}")
+    public PresumptiveDiagnostic getDiagnosticById(@PathVariable(value = "id_diag")Integer id){
+        return presumptiveDiagnosticRepository.findById(id).orElseGet(() -> {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"PresumptiveDiagnostic dont found");
+        });
+    }
+    //agregar  diagnosticos presuntivos
+    @PostMapping
+    public PresumptiveDiagnostic addDiagnostic(@RequestBody PresumptiveDiagnostic presumptiveDiagnostic){
+        return presumptiveDiagnosticRepository.save(presumptiveDiagnostic);
+    }
+
+    //actualizar diagnosticos presuntivos
+    @PutMapping
+    public PresumptiveDiagnostic updateDiagnostic(@RequestBody PresumptiveDiagnostic presumptiveDiagnostic){
+        return presumptiveDiagnosticRepository.save(presumptiveDiagnostic);
+    }
+    // eliminar diagnosticos presuntivos por id
+    @DeleteMapping("/{id_diag}")
+    public void deleteDiagnostic(@PathVariable(name = "id_diag")Integer id){
+        if(presumptiveDiagnosticRepository.findById(id).isPresent()){
+            presumptiveDiagnosticRepository.delete(presumptiveDiagnosticRepository.findById(id).get());
+        }else {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"PresumptiveDiagnostic dont found");
+        }
+    }
+
+}
---
+++ src/main/java/com/anamnesis/AnamnesisService/repository/PresumptiveDiagnosticRepository.java
@@ -0,0 +1,7 @@
+package com.anamnesis.AnamnesisService.repository;
+
+import com.anamnesis.AnamnesisService.model.PresumptiveDiagnostic;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PresumptiveDiagnosticRepository extends CrudRepository<PresumptiveDiagnostic,Integer> {
+}
