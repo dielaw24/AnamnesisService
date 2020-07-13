1e855ca4ef39222ca2935298d7543bf033227edf
dielaw24 <dielaw24@users.noreply.github.com>
2020-07-12 20:12:24 -0500

    limite 8 sintomas getAllsintomas

--- src/main/java/com/anamnesis/AnamnesisService/repository/SymptomRepository.java
+++ src/main/java/com/anamnesis/AnamnesisService/repository/SymptomRepository.java
@@ -1,5 +1,6 @@
 package com.anamnesis.AnamnesisService.repository;
 
+import com.anamnesis.AnamnesisService.model.Disease;
 import com.anamnesis.AnamnesisService.model.Symptom;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -10,4 +11,6 @@ import java.util.List;
 public interface SymptomRepository extends CrudRepository<Symptom,String> {
     @Query(value = "select * from sintoma where nombre_sintoma LIKE CONCAT('%',UPPER(:nombre),'%')", nativeQuery = true)
     List<Symptom> findBySymptomName(@Param("nombre") String nombre);
+    @Query(value = "select * from sintoma limit 8", nativeQuery = true)
+    List<Symptom> findAll();
 }
