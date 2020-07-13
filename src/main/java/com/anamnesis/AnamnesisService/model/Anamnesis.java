33da0326d179ba3d6674c185e6f19d5e43b99939
Juan Moyano <juan.cranex@hotmail.com>
2020-07-12 20:03:06 -0500

    Post de anamnesis final

--- src/main/java/com/anamnesis/AnamnesisService/model/Anamnesis.java
+++ src/main/java/com/anamnesis/AnamnesisService/model/Anamnesis.java
@@ -31,28 +31,23 @@ public class Anamnesis {
     //antecedentes personales patologicos
     @ManyToOne
     @JoinColumn(name = "id_antecedente_p_patologico")
-    @JsonIgnore
     private PathologicalPersonalHistory pathological_personal_history;
     //antecedentes personales no patologicos
     @ManyToOne
     @JoinColumn(name = "id_antecedente_p_no_patologico")
-    @JsonIgnore
     private NoPathologicalPersonalHistory no_pathological_personal_history;
     //condiciones vivienda
     @ManyToOne
     @JoinColumn(name = "id_condiciones_vivienda")
-    @JsonIgnore
     private HousingConditions housing_conditions;
     //Relaciones ManyToMany
 
     //Relacion antecedentes familiares / clase Family
     @OneToMany(mappedBy = "anamnesis")
-    @JsonIgnore
     private List<FamilyHistory> familyHistoryList;
 
     //Relacion antecedentes laborales / clases work y work_history
     @OneToMany(mappedBy = "anamnesis")
-    @JsonIgnore
     private List<WorkHistory> workHistoryList;
 
     //Relacion diagnostico presuntivo / clases Disease y PresumptiveDiagnostic
