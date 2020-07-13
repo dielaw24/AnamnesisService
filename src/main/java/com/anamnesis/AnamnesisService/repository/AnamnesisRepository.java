cc4ae31b0375eb849990adb97e5382a8c2aa6c38
dielaw24 <dielaw24@users.noreply.github.com>
2020-07-12 22:50:51 -0500

    agregado busqyeda de anamnesis por paciente

--- src/main/java/com/anamnesis/AnamnesisService/repository/AnamnesisRepository.java
+++ src/main/java/com/anamnesis/AnamnesisService/repository/AnamnesisRepository.java
@@ -1,7 +1,13 @@
 package com.anamnesis.AnamnesisService.repository;
 
 import com.anamnesis.AnamnesisService.model.Anamnesis;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface AnamnesisRepository extends CrudRepository<Anamnesis,Integer> {
+    @Query(value = "select * from anamnesis where id_paciente =:id", nativeQuery = true)
+    List<Anamnesis> findByPatientId(@Param("id") Integer id);
 }
